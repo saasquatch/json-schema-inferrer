4620ad1a13be66024486d1564b5961d1e08fcea9
slisaasquatch <sli@saasquat.ch>
2019-10-28 09:39:46 -0700

    basic stuff working

--- src/main/java/com/saasquatch/json_schema_inferrer/JsonSchemaInferrer.java
+++ src/main/java/com/saasquatch/json_schema_inferrer/JsonSchemaInferrer.java
@@ -5,10 +5,12 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.apache.commons.validator.routines.EmailValidator;
@@ -52,41 +54,24 @@ public final class JsonSchemaInferrer {
    * @return the inferred JSON schema
    */
   public ObjectNode infer(@Nullable JsonNode input) {
-    final ObjectNode processOutput;
-    final ObjectNode output = newObject();
-    if (draft != null && outputDollarSchema) {
-      output.put(Fields.SCHEMA, draft.url);
-    }
-    if (title != null) {
-      output.put(Fields.TITLE, title);
-    }
-
     if (input == null) {
       input = JsonNodeFactory.instance.nullNode();
     }
-    if (input.isObject()) {
-      processOutput = processObject(input, null, false);
-      output.put(Fields.TYPE, processOutput.path(Fields.TYPE).textValue());
-      output.set(Fields.PROPERTIES, processOutput.get(Fields.PROPERTIES));
-    } else if (input.isArray()) {
-      processOutput = processArray(input, null, false);
-      output.put(Fields.TYPE, processOutput.path(Fields.TYPE).textValue());
-      output.set(Fields.ITEMS, processOutput.get(Fields.ITEMS));
-
-      if (nonNull(output.get(Fields.TITLE))) {
-        final String outputTitle = output.get(Fields.TITLE).textValue();
-        ((ObjectNode) output.get(Fields.ITEMS)).put(Fields.TITLE, outputTitle);
-        output.put(Fields.TITLE, outputTitle + " Set");
-      }
+    final ObjectNode result;
+    if (input instanceof ObjectNode) {
+      result = processObject((ObjectNode) input);
+    } else if (input instanceof ArrayNode) {
+      result = processArray((ArrayNode) input);
     } else {
-      output.put(Fields.TYPE, getPropertyType(input));
-      final String format = getPropertyFormat(input);
-      if (format != null && !format.isEmpty()) {
-        output.put(Fields.FORMAT, format);
-      }
+      result = processPrimitive((ValueNode) input);
     }
-
-    return output;
+    if (draft != null && outputDollarSchema) {
+      result.put(Fields.SCHEMA, draft.url);
+    }
+    if (title != null) {
+      result.put(Fields.TITLE, title);
+    }
+    return result;
   }
 
   @Nullable
@@ -162,204 +147,6 @@ public final class JsonSchemaInferrer {
         String.format(Locale.ROOT, "Unrecognized %s: %s", type.getClass().getSimpleName(), type));
   }
 
-  private ArrayNode getUniqueKeys(JsonNode aInput, JsonNode bInput, JsonNode cInput) {
-    final List<String> a = iteratorToList(aInput.fieldNames());
-    final List<String> b = iteratorToList(bInput.fieldNames());
-    final List<String> c;
-    if (cInput == null) {
-      c = new ArrayList<>();
-    } else {
-      c = toStringList(cInput);
-    }
-
-    for (String value : b) {
-      final int aIndex = a.indexOf(value);
-      final int cIndex = c.indexOf(value);
-      if (aIndex == -1) {
-        if (cIndex != -1) {
-          // Value is optional, it doesn't exist in A but exists in B(n)
-          c.remove(cIndex);
-        }
-      } else if (cIndex == -1) {
-        // Value is required, it exists in both B and A, and is not yet present in C
-        c.add(value);
-      }
-    }
-    final ArrayNode resultArray = newArray();
-    for (String _c : c) {
-      resultArray.add(_c);
-    }
-    return resultArray;
-  }
-
-  private ObjectNode processArray(@Nonnull JsonNode array, @Nullable ObjectNode output,
-      boolean nested) {
-    String format = null;
-    boolean oneOf = false;
-    String type = null;
-    if (nested && nonNull(output)) {
-      final ObjectNode newOutput = newObject();
-      newOutput.set(Fields.ITEMS, output);
-      output = newOutput;
-    } else {
-      if (output == null || !output.isObject()) {
-        output = newObject();
-      }
-      output.put(Fields.TYPE, getPropertyType(array));
-      if (!output.path(Fields.ITEMS).isObject()) {
-        output.set(Fields.ITEMS, newObject());
-      }
-      type = output.path(Fields.ITEMS).path(Fields.TYPE).textValue();
-    }
-
-    // Determine whether each item is different
-    for (JsonNode value : array) {
-      final String elementType = getPropertyType(value);
-      final String elementFormat = getPropertyFormat(value);
-      if (type != null && !type.isEmpty() && !elementType.equals(type)) {
-        ((ObjectNode) output.get(Fields.ITEMS)).set(Fields.ONE_OF, newArray());
-        oneOf = true;
-        break;
-      } else {
-        type = elementType;
-        format = elementFormat;
-      }
-    }
-
-    // Setup type otherwise
-    if (!oneOf && (type != null && !type.isEmpty())) {
-      ((ObjectNode) output.path(Fields.ITEMS)).put(Fields.TYPE, type);
-      if (format != null && !format.isEmpty()) {
-        ((ObjectNode) output.path(Fields.ITEMS)).put(Fields.FORMAT, format);
-      }
-    } else if (oneOf && !Types.OBJECT.equals(type)) {
-      final ObjectNode outputItems = newObject();
-      outputItems.set(Fields.ONE_OF, arrayNodeOf(newObject().put(Fields.TYPE, type)));
-      final JsonNode originalRequired = output.path(Fields.ITEMS).get(Fields.REQUIRED);
-      if (nonNull(originalRequired)) {
-        outputItems.set(Fields.REQUIRED, originalRequired);
-      }
-      (output).set(Fields.ITEMS, outputItems);
-    }
-
-    // Process each item depending
-    if (output.path(Fields.ITEMS).get(Fields.ONE_OF) != null || Types.OBJECT.equals(type)) {
-      for (JsonNode value : array) {
-        final String itemType = getPropertyType(value);
-        final String itemFormat = getPropertyFormat(value);
-        ObjectNode arrayItem;
-        if (Types.OBJECT.equals(itemType)) {
-          if (output.path(Fields.ITEMS).get(Fields.PROPERTIES) != null) {
-            ((ObjectNode) output.path(Fields.ITEMS)).set(Fields.REQUIRED,
-                getUniqueKeys(output.path(Fields.ITEMS).get(Fields.PROPERTIES), value,
-                    output.path(Fields.ITEMS).get(Fields.REQUIRED)));
-          }
-          arrayItem = processObject(value,
-              oneOf ? newObject() : (ObjectNode) output.path(Fields.ITEMS).get(Fields.PROPERTIES),
-              true);
-        } else if (Types.ARRAY.equals(itemType)) {
-          arrayItem = processArray(value,
-              oneOf ? newObject() : (ObjectNode) output.path(Fields.ITEMS).get(Fields.PROPERTIES),
-              true);
-        } else {
-          arrayItem = newObject().put(Fields.TYPE, itemType);
-          if (itemFormat != null && !itemFormat.isEmpty()) {
-            arrayItem.put(Fields.FORMAT, itemFormat);
-          }
-        }
-        if (oneOf) {
-          final String childType = getPropertyType(value);
-          final ObjectNode tempObj = newObject();
-          if (arrayItem.path(Fields.TYPE).textValue() == null && Types.OBJECT.equals(childType)) {
-            tempObj.set(Fields.PROPERTIES, arrayItem);
-            tempObj.put(Fields.TYPE, Types.OBJECT);
-            arrayItem = tempObj;
-          }
-          final ArrayNode itemsOneOf = (ArrayNode) output.path(Fields.ITEMS).get(Fields.ONE_OF);
-          if (!contains(itemsOneOf, arrayItem)) {
-            itemsOneOf.add(arrayItem);
-          }
-        } else {
-          if (!Types.OBJECT.equals(output.path(Fields.ITEMS).path(Fields.TYPE).textValue())) {
-            continue;
-          }
-          ((ObjectNode) output.get(Fields.ITEMS)).set(Fields.PROPERTIES, arrayItem);
-        }
-      }
-    }
-    return nested ? (ObjectNode) output.get(Fields.ITEMS) : output;
-  }
-
-  private ObjectNode processObject(@Nonnull JsonNode object, @Nullable ObjectNode output,
-      boolean nested) {
-    if (nested && nonNull(output)) {
-      final ObjectNode newOutput = newObject();
-      newOutput.set(Fields.PROPERTIES, output);
-      output = newOutput;
-    } else {
-      if (output == null || !output.isObject()) {
-        output = newObject();
-      }
-      output.put(Fields.TYPE, getPropertyType(object));
-      if (!output.path(Fields.PROPERTIES).isObject()) {
-        output.set(Fields.PROPERTIES, newObject());
-      }
-    }
-
-    for (String key : iteratorToList(object.fieldNames())) {
-      final JsonNode value = object.get(key);
-      final String type = getPropertyType(value);
-      final String format = getPropertyFormat(value);
-
-      if (type.equals(Types.OBJECT)) {
-        ((ObjectNode) output.get(Fields.PROPERTIES)).set(key,
-            processObject(value, (ObjectNode) output.path(Fields.PROPERTIES).get(key), false));
-        continue;
-      }
-
-      if (type.equals(Types.ARRAY)) {
-        ((ObjectNode) output.get(Fields.PROPERTIES)).set(key,
-            processArray(value, (ObjectNode) output.path(Fields.PROPERTIES).get(key), false));
-        continue;
-      }
-
-      if (nonNull(output.path(Fields.PROPERTIES).get(key))) {
-        final ObjectNode entry = (ObjectNode) output.path(Fields.PROPERTIES).get(key);
-        final boolean hasTypeArray = entry.path(Fields.TYPE).isArray();
-
-        /*
-         * When an array already exists, we check the existing type array to see if it contains our
-         * current property type, if not, we add it to the array and continue
-         */
-        if (hasTypeArray) {
-          if (toStringList(entry.get(Fields.TYPE)).contains(type)) {
-            ((ArrayNode) entry.get(Fields.TYPE)).add(type);
-          }
-        }
-
-        /*
-         * When multiple fields of differing types occur, json schema states that the field must
-         * specify the primitive types the field allows in array format.
-         */
-        if (!hasTypeArray && !type.equals(entry.path(Fields.TYPE).textValue())) {
-          final ArrayNode newEntryType = newArray();
-          newEntryType.add(entry.get(Fields.TYPE));
-          newEntryType.add(type);
-          entry.set(Fields.TYPE, newEntryType);
-        }
-
-        continue;
-      }
-
-      ((ObjectNode) output.get(Fields.PROPERTIES)).set(key, newObject().put(Fields.TYPE, type));
-
-      if (format != null && !format.isEmpty()) {
-        ((ObjectNode) output.path(Fields.PROPERTIES).get(key)).put(Fields.FORMAT, format);
-      }
-    }
-    return nested ? (ObjectNode) output.get(Fields.PROPERTIES) : output;
-  }
-
   private ObjectNode processPrimitive(ValueNode valueNode) {
     final ObjectNode result = newObject();
     result.put(Fields.TYPE, getPropertyType(valueNode));
@@ -371,7 +158,6 @@ public final class JsonSchemaInferrer {
   }
 
   private ObjectNode processObject(ObjectNode objectNode) {
-    final ObjectNode result = newObject();
     final ObjectNode properties = newObject();
     final Iterator<Map.Entry<String, JsonNode>> fields = objectNode.fields();
     while (fields.hasNext()) {
@@ -386,6 +172,7 @@ public final class JsonSchemaInferrer {
         properties.set(key, processPrimitive((ValueNode) val));
       }
     }
+    final ObjectNode result = newObject().put(Fields.TYPE, Types.OBJECT);
     if (properties.size() > 0) {
       result.set(Fields.PROPERTIES, properties);
     }
@@ -394,7 +181,18 @@ public final class JsonSchemaInferrer {
 
   private ObjectNode processArray(ArrayNode arrayNode) {
     final ObjectNode items;
+    final Set<ObjectNode> oneOfRaw = new LinkedHashSet<>();
+    for (JsonNode val : arrayNode) {
+      if (val instanceof ObjectNode) {
+        oneOfRaw.add(processObject((ObjectNode) val));
+      } else if (val instanceof ArrayNode) {
+        oneOfRaw.add(processArray((ArrayNode) val));
+      } else {
+        oneOfRaw.add(processPrimitive((ValueNode) val));
+      }
+    }
     final ArrayNode oneOf = newArray();
+    oneOfRaw.forEach(oneOf::add);
     switch (oneOf.size()) {
       case 0:
         items = newObject();
@@ -407,7 +205,7 @@ public final class JsonSchemaInferrer {
         items.set(Fields.ONE_OF, oneOf);
       }
     }
-    final ObjectNode result = newObject();
+    final ObjectNode result = newObject().put(Fields.TYPE, Types.ARRAY);
     result.set(Fields.ITEMS, items);
     return result;
   }
@@ -474,21 +272,13 @@ public final class JsonSchemaInferrer {
   }
 
   private static interface Fields {
-    String TYPE = "type", ITEMS = "items", ONE_OF = "oneOf", REQUIRED = "required",
+    String TYPE = "type", ITEMS = "items", ONE_OF = "oneOf", /* REQUIRED = "required", */
         PROPERTIES = "properties", FORMAT = "format", SCHEMA = "$schema", TITLE = "title";
   }
 
   private static interface Types {
     String OBJECT = "object", ARRAY = "array", STRING = "string", BOOLEAN = "boolean",
         INTEGER = "integer", NUMBER = "number", NULL = "null";
-  }
-
-  private static ArrayNode arrayNodeOf(JsonNode... nodes) {
-    final ArrayNode result = JsonNodeFactory.instance.arrayNode();
-    for (JsonNode node : nodes) {
-      result.add(node);
-    }
-    return result;
   }
 
   private static boolean nonNull(@Nullable JsonNode j) {
@@ -503,14 +293,6 @@ public final class JsonSchemaInferrer {
     return JsonNodeFactory.instance.arrayNode();
   }
 
-  private static <T> List<T> iteratorToList(Iterator<T> iterator) {
-    final List<T> list = new ArrayList<>();
-    while (iterator.hasNext()) {
-      list.add(iterator.next());
-    }
-    return list;
-  }
-
   // Visible for testing
   static List<String> toStringList(@Nonnull JsonNode arrayNode) {
     final List<String> list = new ArrayList<>();
@@ -518,15 +300,6 @@ public final class JsonSchemaInferrer {
       list.add(node.textValue());
     }
     return list;
-  }
-
-  private static boolean contains(@Nonnull JsonNode arrayNode, @Nonnull JsonNode target) {
-    for (JsonNode node : arrayNode) {
-      if (node.equals(target)) {
-        return true;
-      }
-    }
-    return false;
   }
 
 }
